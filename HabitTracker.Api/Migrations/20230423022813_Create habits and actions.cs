5f8bf670e549a80933ef8f2e07deb6fe97f60766
Bernardo Pires Mesko <bernardo.mesko@ip5tecnologia.com.br>
2023-04-22 23:31:08 -0300

    Add initial migrations

---
+++ HabitTracker.Api/Migrations/20230423022813_Create habits and actions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HabitTracker.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class Createhabitsandactions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Habits",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Habits", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HabitActions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ActionTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    HabitId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HabitActions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HabitActions_Habits_HabitId",
+                        column: x => x.HabitId,
+                        principalTable: "Habits",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HabitActions_HabitId",
+                table: "HabitActions",
+                column: "HabitId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HabitActions");
+
+            migrationBuilder.DropTable(
+                name: "Habits");
+        }
+    }
+}
